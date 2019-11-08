cf36d68a99494b05806cdb0336f4de0ded7000e5
Anthony Dellinger <adell892@live.kutztown.edu>
2019-11-08 11:20:15 -0500

    started work on closure scope management

--- tests/test_P_meta_coord.jl
+++ tests/test_P_meta_coord.jl
@@ -2,7 +2,7 @@ function test_with_ga(;verbose::Int=0)
 	for dataset in [7]
 	    problems = parse_file("../benchmark_problems/mdmkp_ct$(dataset).txt")
 
-	    for p in 5:length(problems)
+	    for p in 1:20
 			problem = problems[p]
 	        pop::Population = greedy_construct(problem, 90, ls=local_swap, force_valid=false)
 			first_sum = sum(get_population_scores(pop))
@@ -22,7 +22,33 @@ function test_with_ga(;verbose::Int=0)
 	end
 end
 
-test_with_ga()
+function test_perturb_closure()
+	problems = parse_file("../benchmark_problems/mdmkp_ct3.txt")
+	GA_3samp = PMCPC(column_average_chances, Dict{Symbol,Int}(:n_samples => 3))
+	GA_1samp = PMCPC(column_average_chances, Dict{Symbol,Int}())
+
+	for p in 1:20
+		problem = problems[p]
+		pop::Population = random_init(problem, 90, ls=local_swap, force_valid=false)
+		first_sum = sum(get_population_scores(pop))
+
+		GA_3samp_results = deepcopy(pop)
+		P_meta_coord(GA_3samp_results, problem, VND, GA_3samp, use_random=true, random_n=1, time_limit=.1)
+
+		GA_1samp_results = deepcopy(pop)
+		# P_meta_coord(GA_1samp_results, problem, local_swap, GA_1samp, use_random=true, random_n=1, time_limit=.1)
+
+		GA_results = deepcopy(pop)
+		# P_meta_coord(GA_results, problem, local_swap, column_average_chances, use_random=true, random_n=1, time_limit=.1)
+
+		println("")
+		second_sum = sum(get_population_scores(pop))
+		# @assert second_sum > first_sum
+	end
+end
+
+# test_with_ga()
+test_perturb_closure()
 # using Profile
 # Profile.clear()
 # @profile test()
--- tests/test_initial_pop.jl
+++ tests/test_initial_pop.jl
@@ -1,25 +1,56 @@
-function test_all_feasible()
+function greedy_test_all_feasible()
 	for dataset in [2]
 	    problems = parse_file("../benchmark_problems/mdmkp_ct$(dataset).txt")
 
-	    for problem in problems
-	        pop = greedy_construct(problem, 30, ls=make_solution, max_time=10)
+	    for problem in problems[1:20]
+	        pop = greedy_construct(problem, 3, ls=make_solution, max_time=10)
 			assure_unique(pop)
 		end
 	end
 end
 
-function test_some_infeasible()
+function greedy_test_some_infeasible()
 	for dataset in [7]
 	    problems = parse_file("../benchmark_problems/mdmkp_ct$(dataset).txt")
 
-	    for problem in problems
+	    for problem in problems[1:20]
 	        pop = greedy_construct(problem, 30, ls=make_solution, max_time=1, force_valid=false)
 			assure_unique(pop)
 		end
 	end
 end
 
-test_all_feasible()
-test_some_infeasible()
-println("initial pop tests passed")
+function random_test_all_feasible()
+	for dataset in [4]
+	    problems = parse_file("../benchmark_problems/mdmkp_ct$(dataset).txt")
+
+	    for problem in problems[1:20]
+	        pop = random_init(problem, 3, ls=VND, max_time=10)
+			assure_unique(pop)
+		end
+	end
+end
+
+function random_test_some_infeasible()
+	for dataset in [9]
+	    problems = parse_file("../benchmark_problems/mdmkp_ct$(dataset).txt")
+
+	    for problem in problems[1:20]
+	        pop = random_init(problem, 30, ls=make_solution, max_time=1, force_valid=false)
+			assure_unique(pop)
+		end
+	end
+end
+
+printstyled("testing initial pop methods...\n",color=:blue)
+greedy_test_all_feasible()
+printstyled("   greedy all feasible passed. \n", color=:light_black)
+greedy_test_some_infeasible()
+printstyled("   greedy no feas constraint passed. \n", color=:light_black)
+
+random_test_all_feasible()
+printstyled("   random init all feasible embedded VND passed. \n", color=:light_black)
+random_test_some_infeasible()
+printstyled("   random initi no feas constraint passed. \n", color=:light_black)
+
+printstyled("initial pop tests passed. \n", color=:green)
