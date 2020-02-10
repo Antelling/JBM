dd023a4fd690459e28f2f3f58466833008d662e4
Anthony Dellinger <adell892@live.kutztown.edu>
2020-02-10 10:22:59 -0500

    created impgen recorder and results

--- main.jl
+++ main.jl
@@ -15,7 +15,7 @@ end
 
 function generate_rao1_narrow_survey(; popsize::Int=30, time_limit::Int=15)
     configurations = Vector{Tuple{String,Any}}()
-    push!(configurations, tuple("control", identity))
+    push!(configurations, tuple("control", pmeta_control))
     for (search_name, search) in [("fast local search", FLS), ("no local search", make_solution)]
         for n in [1, 5, Int(popsize/2)]
             title = "Rao1 top$n bottom$n " * search_name
@@ -42,10 +42,11 @@ function main(; time_limit::Number=10, popsize::Int=30, generator::Function, exp
     run(`mkdir -p results/$(experiment_name)`)
     metaheuristics = generator(popsize=popsize, time_limit=time_limit)
 
-    for dataset in 1:9
-        problems = parse_file("./benchmark_problems/mdmkp_ct$(dataset).txt")
+    for dataset in 1:3
+        problems = parse_file("./benchmark_problems/mdmkp_ct$(dataset).txt", dataset)
 
         results = Dict{String,Vector}()
+		improved_gen_results = Vector{Tuple{Problem_ID, Vector{Tuple{Int,Int}}}}()
         for (name, alg) in metaheuristics
             println(name)
             println(alg)
@@ -57,7 +58,7 @@ function main(; time_limit::Number=10, popsize::Int=30, generator::Function, exp
         for (i, problem) in enumerate(problems)
             println("")
             println("")
-            println("Testing problem number $i (case first order)")
+            println("Testing $(problem.id)")
 
             # generate an initial population using the greedy construct strategy
             # do not force feasibility
@@ -66,8 +67,9 @@ function main(; time_limit::Number=10, popsize::Int=30, generator::Function, exp
             for (name, meta) in metaheuristics
                 curr_pop = deepcopy(pop)
                 start_time = time()
-                meta(curr_pop, problem)
+                improvement_gens = meta(curr_pop, problem)
                 end_time = time()
+				push!(improved_gen_results, tuple(problem.id, improvement_gens))
                 highest = get_population_scores(curr_pop)[end]
                 println("$name found highest score of $highest")
                 push!(results[name], (highest, end_time - start_time))
@@ -75,9 +77,10 @@ function main(; time_limit::Number=10, popsize::Int=30, generator::Function, exp
         end
 
         file = open("results/$(experiment_name)/ds$(dataset)_tl$(time_limit)_laptop.json", "w")
+        file = open("results/$(experiment_name)/ds$(dataset)_tl$(time_limit)_laptop_genimps.json", "w")
         write(file, JSON.json(results))
         close(file)
     end
 end
 
-main(generator=generate_rao1_narrow_survey, experiment_name="rao1_narrow_survey", popsize=30, time_limit=3)
+main(generator=generate_rao1_narrow_survey, experiment_name="junk-test", popsize=30, time_limit=10)
